f118c174cb8cd4a9443245b7c67849454a5df956
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-27 16:21:29 -0400

    get redis db file in working state

--- services/crawler/internal/spider/constants.go
+++ services/crawler/internal/spider/constants.go
@@ -1,5 +1,12 @@
 package spider
 
+import "time"
+
 const (
+	Timeout = 4 * time.Second
+
+	IndexerQueueKey     = "indexer_queue"
+	SignalQueueKey      = "signal_queue"
+	CrawlerQueueKey     = "crawler_queue"
 	NormalizedURLPrefix = "normalized_url"
 )
