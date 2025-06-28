fff63effb6c4fa4397a3a6d589b8b0669fc649c6
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-28 15:07:18 -0400

    refactor crawler main.go for redis database

--- services/crawler/internal/crawler_utilities/constants.go
+++ services/crawler/internal/crawler_utilities/constants.go
@@ -3,10 +3,12 @@ package crawler_utilities
 import "time"
 
 const (
-	Timeout = 4 * time.Second
+	Timeout             = 4 * time.Second
+	MaxIndexerQueueSize = 5000
 
 	IndexerQueueKey     = "indexer_queue"
 	SignalQueueKey      = "signal_queue"
 	CrawlerQueueKey     = "crawler_queue"
 	NormalizedURLPrefix = "normalized_url"
+	ResumeCrawl         = "RESUME_CRAWL"
 )
--- services/crawler/spider_main/main.go
+++ services/crawler/spider_main/main.go
@@ -1,52 +1,88 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"os"
-	"strconv"
 
+	"github.com/kawilkinson/search-engine/internal/crawler_utilities"
+	"github.com/kawilkinson/search-engine/internal/pages"
+	"github.com/kawilkinson/search-engine/internal/redis_db"
 	"github.com/kawilkinson/search-engine/internal/spider"
 )
 
 func main() {
-	arguments := os.Args[1:]
-	if len(arguments) < 1 {
-		fmt.Println("no website provided")
-		os.Exit(1)
-	} else if len(arguments) > 3 {
-		fmt.Println("too many arguments provided")
-		os.Exit(1)
-	}
+	maxConcurrency := flag.Int("max-concurrency", 10, "Max number of concurrent channels")
+	maxPages := flag.Int("max-pages", 100, "Max number of pages able to be done at once")
+	flag.Parse()
 
-	baseURL := arguments[0]
-	maxConcurrency := 5
-	maxPages := 100
+	redisHost := loadEnv("REDIS_HOST", "localhost")
+	redisPort := loadEnv("REDIS_PORT", "6379")
+	redisPassword := loadEnv("REDIS_PASSWORD", "")
+	redisDB := loadEnv("REDIS_DB", "0")
+	startingURL := loadEnv("STARTING_URL", "https://en.wikipedia.org/wiki/Japan")
+	baseURL := "https://en.wikipedia.org"
 
-	if len(arguments) >= 2 {
-		if val, err := strconv.Atoi(arguments[1]); err == nil {
-			maxConcurrency = val
-		} else {
-			fmt.Println("Invalid maxConcurrency value provided, using default of 5")
-		}
-	}
-	if len(arguments) >= 3 {
-		if val, err := strconv.Atoi(arguments[2]); err == nil {
-			maxPages = val
-		} else {
-			fmt.Println("Invalid maxPages value provided, using default of 100")
-		}
-	}
-
-	cfg, err := spider.Configure(baseURL, maxConcurrency, maxPages)
+	db := &redis_db.RedisDatabase{}
+	err := db.ConnectToRedis(redisHost, redisPort, redisPassword, redisDB)
 	if err != nil {
-		log.Fatalf("error configuring WebGopher: %v", err)
+		log.Printf("unable to connect to redis database: %v\n", err)
 	}
 
-	cfg.Wg.Add(1)
-	fmt.Printf("starting crawl of %s...\n", baseURL)
-	go cfg.CrawlPage(baseURL)
-	cfg.Wg.Wait()
+	db.PushURLToQueue(startingURL, 0)
+	log.Printf("starting queue with %v\n", startingURL)
 
-	spider.PrintReport(cfg.Pages, baseURL)
+	// create controllers and initialize them here
+
+	cfg, err := spider.Configure(baseURL, *maxConcurrency, *maxPages)
+	if err != nil {
+		log.Fatalf("error configuring crawler: %v", err)
+	}
+
+	// Main loop for the crawler to continuously run through URLs and push to Redis
+	for {
+		log.Printf("Checking number of entries in queue...")
+		queueSize, err := db.GetIndexerQueueSize()
+		if err != nil {
+			log.Printf("unable to get indexer queue size: %v\n", err)
+			return
+		}
+
+		if queueSize >= crawler_utilities.MaxIndexerQueueSize {
+			log.Printf("indexer queue is full, waiting...\n")
+			for {
+				signal, err := db.PopSignalQueue()
+				if err != nil {
+					log.Printf("unable to get signal from queue: %v\n", err)
+					return
+				}
+
+				if signal == crawler_utilities.ResumeCrawl {
+					log.Println("resuming crawl of web pages")
+					break
+				}
+			}
+		}
+
+		log.Println("creating workers...")
+		cfg.Wg.Add(1)
+		go cfg.CrawlPage(db)
+		cfg.Wg.Wait()
+
+		// Use controllers to write entries to Redis database
+
+		cfg.Pages = make(map[string]*pages.Page)
+		cfg.Outlinks = make(map[string]*pages.PageNode)
+		cfg.Backlinks = make(map[string]*pages.PageNode)
+		cfg.Images = make(map[string][]*pages.Image)
+	}
+
+}
+
+func loadEnv(key, fallback string) string {
+	if envVariable, exists := os.LookupEnv(key); exists {
+		return envVariable
+	}
+
+	return fallback
 }
