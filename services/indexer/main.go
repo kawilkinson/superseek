fc6d0ef063e4ec00752e3f28c11e421c0da6b030
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-07-01 18:22:53 -0400

    begin indexer and its redisdb functions

--- services/indexer/main.go
+++ services/indexer/main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+
+type RedisDatabase struct {
+	Client  *redis.Client
+	Context context.Context
+}
+
 // entry into indexer service, a core service that parses the data from the crawler
 func main() {
+	redisHost := loadEnv("REDIS_HOST", "localhost")
+	redisPort := loadEnv("REDIS_PORT", "6379")
+	redisPassword := loadEnv("REDIS_PASSWORD", "")
+	redisDB := loadEnv("REDIS_DB", "0")
 
+	
+
+}
+
+func loadEnv(key, fallback string) string {
+	if envVariable, exists := os.LookupEnv(key); exists {
+		return envVariable
+	}
+
+	return fallback
 }
