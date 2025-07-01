d182eaee2815c2e57cd6e087ef01d389febfecd6
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-07-01 18:28:28 -0400

    refactor indexer redis db functions

--- services/indexer/internal/redisdb/redis_page_data.go
+++ services/indexer/internal/redisdb/redis_page_data.go
@@ -42,21 +42,6 @@ func (db *RedisDatabase) PeekPage() string {
 	return pageID
 }
 
-func (db *RedisDatabase) GetQueueSize() int64 {
-	size, err := db.Client.LLen(db.Context, indexerutil.IndexerQueueKey).Result()
-	if err != nil {
-		log.Printf("unable to get %v's size: %v", indexerutil.IndexerQueueKey, err)
-		return -1
-	}
-
-	return size
-}
-
-func (db *RedisDatabase) SignalCrawler() {
-	db.Client.LPush(db.Context, indexerutil.SignalQueueKey, indexerutil.ResumeCrawl)
-	log.Println("signaled crawler")
-}
-
 func (db *RedisDatabase) GetPageData(key string) *pages.Page {
 	pageHashed, err := db.Client.HGetAll(db.Context, key).Result()
 	if err != nil {
@@ -87,47 +72,4 @@ func (db *RedisDatabase) DeletePageData(key string) {
 		log.Printf("could not remove page data for %s from Redis", key)
 		return
 	}
-}
-
-func (db *RedisDatabase) GetOutlinks(normalizedURL string) *pages.Outlinks {
-	key := indexerutil.OutlinksPrefix + ":" + normalizedURL
-	result, err := db.Client.SMembers(db.Context, key).Result()
-	if err != nil {
-		log.Printf("unable to get outlinks from key %s: %v", key, err)
-		return nil
-	}
-
-	if len(result) == 0 {
-		log.Printf("unable to get outlinks, no outlinks found for %s", key)
-		return nil
-	}
-
-	linksSet := make(map[string]struct{}, len(result))
-	for _, link := range result {
-		linksSet[link] = struct{}{}
-	}
-
-	return &pages.Outlinks{
-		ID:    normalizedURL,
-		Links: linksSet,
-	}
-}
-
-func (db *RedisDatabase) DeleteOutlinks(normalizedURL string) {
-	key := indexerutil.OutlinksPrefix + ":" + normalizedURL
-	result, err := db.Client.Del(db.Context, key).Result()
-	if err != nil {
-		log.Printf("unable to delete outlinks from key %s: %v", key, err)
-		return
-	}
-
-	if result <= 0 {
-		log.Printf("could not remove outlinks for %s from Redis", key)
-		return
-	}
-}
-
-func (db *RedisDatabase) PushToImageIndexerQueue(normalizedURL string) {
-	db.Client.LPush(db.Context, indexerutil.IndexerQueueKey, normalizedURL)
-	log.Printf("pushed %s to image indexer queue", normalizedURL)
 }
