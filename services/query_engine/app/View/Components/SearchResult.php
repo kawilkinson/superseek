acb09f9522555588da9cc0a9f36ab35c8727a642
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-07-21 22:05:27 -0400

    create all Laravel components for search engine

--- services/query_engine/app/View/Components/SearchResult.php
+++ services/query_engine/app/View/Components/SearchResult.php
@@ -6,25 +6,21 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class ImageContainer extends Component
+class SearchResult extends Component
 {
     public $url;
-    public $alt;
     public $title;
-    public $page_url;
     public $text;
 
-    public function __construct($url, $alt, $title, $page, $text)
-    {   
+    public function __construct($url, $title, $text)
+    {
         $this->url = $url;
-        $this->alt = $alt;
         $this->title = $title;
-        $this->page_url = $page;
         $this->text = $text;
     }
 
     public function render(): View|Closure|string
     {
-        return view('components.image-container');
+        return view('components.search-result');
     }
 }
