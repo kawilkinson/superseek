5321b2e534e3b39fd83f87a924870b740c4e3e73
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-07-23 17:37:08 -0400

    create random-result and image-container components

---
+++ services/query_engine/resources/views/components/image-container.blade.php
@@ -0,0 +1,9 @@
+<a href="https://{{ $page_url }}" target="_blank">
+    <div class="image">
+        <div class="flex justify-center w-full actual-image">
+            <img src="https://{{ $url }}" alt="{{ $alt }}" class="w-full max-w-full object-contain">
+        </div>
+        <p class="img-title">{{ $title }}</p>
+        <p class="img-text">{{ $text }}</p>
+    </div>
+</a>
---
+++ services/query_engine/resources/views/components/random-result.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <!-- Nothing of note here for this component, but leaving in case I want to add anything later -->
+</div>
